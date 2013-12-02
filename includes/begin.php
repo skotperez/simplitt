2706b50e05111bb99530e0d0918031396372098e
skotperez <voraginebramante@gmail.com>
2013-12-02 11:24:09 +0100

    fix echo problem with meta data in site header

--- includes/begin.php
+++ includes/begin.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 
-<html lang="<?php sptt_get_site_metadata("lang"); ?>">
+<html lang="<?php echo sptt_get_site_metadata("lang"); ?>">
 
 <head>
 <meta charset="UTF-8" />
-<title><?php sptt_get_site_metadata("title"); ?></title>
+<title><?php echo sptt_get_site_metadata("title"); ?></title>
 
-<meta content="<?php sptt_get_site_metadata("title"); ?>" name="description" />
+<meta content="<?php echo sptt_get_site_metadata("title"); ?>" name="description" />
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <link rel="stylesheet" type="text/css" href="style.css" />
